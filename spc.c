4f461c67b01d2e4145d93545204d0d401ccf79ff
hooiv <125276621+hooiv@users.noreply.github.com>
2025-01-28 11:18:44 +0000

    Jan 28, 2025, 4:48 PM

--- spc.c
+++ spc.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 int main(int argc,char *argv[]){
 if (argc<4){
-    prinf("");
-    printf("");;
+    printf("");
+    printf("");
 }
+char ip[16]={0};
+strcpy(ip,argv[1]);
+
 }
